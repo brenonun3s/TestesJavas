a95c27abde4a7e45225422da2f1a89b67fd49380
Breno Nunes <166417202+brenonun3s@users.noreply.github.com>
2024-08-20 17:19:05 -0300

    Add files via upload

---
+++ ExemploBreakeContinue.java
@@ -0,0 +1,12 @@
+public class ExemploBreakeContinue {
+    
+    public static void main(String[] args) {
+
+        String alunos [] = {"Breno", "Jonas", "Pedro"};
+
+        for(String aluno : alunos){
+        System.out.println("Nome do aluno é:" + aluno);
+        }
+    }
+}
+
---
+++ ExemploWhile.java
@@ -0,0 +1,26 @@
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ExemploWhile {
+
+    public static void main(String[] args) {
+        double mesada = 50.0;
+
+        while(mesada > 0){
+            Double valorDoce = valorAleatorio();
+            if(valorDoce > mesada)
+            valorDoce = mesada;
+
+            System.out.println("Doce de valor:" + valorDoce + "Adicionado no carrinho");
+            mesada = mesada - valorDoce;
+
+        }
+    
+        System.out.println("Mesdada" + mesada);
+        System.out.println("Joãozinho gastou toda sua mesada em doces");
+    }
+
+    private static double valorAleatorio() {
+        return ThreadLocalRandom.current().nextDouble(2,8);
+
+    }
+}
